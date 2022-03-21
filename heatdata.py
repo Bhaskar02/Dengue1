ce6669345329e84dc7fd81692a2c23d5c123ee32
Bhaskar02 <bhaskarrao02@gmail.com>
2022-03-21 15:09:44 +0530

    Update heatdata.py

--- heatdata.py
+++ heatdata.py
@@ -8,12 +8,12 @@ import numpy as np
 #import coiled
 #import dask
 #import dask.dataframe as dd
-import folium
+#import folium
 import streamlit as st
 #from dask.distributed import Client
-from folium.plugins import HeatMap
+#from folium.plugins import HeatMap
 from streamlit_folium import folium_static
-from folium.plugins import HeatMap
+from streamlit_folium.plugins import HeatMap
 import pandas as pd
 import streamlit as st
 import leafmap.foliumap as leafmap
